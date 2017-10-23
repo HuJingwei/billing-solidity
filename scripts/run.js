c598adfcefc367ddedd371d000177f7266a6c3ff
HuJingwei <a479540195@gmail.com>
2017-10-23 11:10:28 +0800

    test script

--- scripts/run.js
+++ scripts/run.js
@@ -20,9 +20,20 @@ this.bill = contract.at(contractAddress);
 const attContract = this.web3.eth.contract(this.attContractsAbi);
 this.att = attContract.at(attAdd);
 // console.log(this.att);
-// this.att.generateTokens.sendTransaction("0x3ae88fe370c39384fc16da2c9e768cf5d2495b48",33,{from:"0x3ae88fe370c39384fc16da2c9e768cf5d2495b48",gas:3000000});
-// this.att.approve.sendTransaction("0x832fc6b937d9b3f4617bc47ee5931161196c5825", 100,{from:"0x3ae88fe370c39384fc16da2c9e768cf5d2495b48",gas:4712387});
-// console.log(this.att.allowance("0x3ae88fe370c39384fc16da2c9e768cf5d2495b48","0x832fc6b937d9b3f4617bc47ee5931161196c5825",{from:"0x3ae88fe370c39384fc16da2c9e768cf5d2495b48",gas:3000000}))
-// console.log(this.att.balanceOf("0x3ae88fe370c39384fc16da2c9e768cf5d2495b48"))
-// this.bill.billing(owner);
-// this.bill.deductFee();
+// this.att.generateTokens.sendTransaction(owner,33,{from:owner,gas:3000000});
+// this.att.approve.sendTransaction(contractAddress, 1000,{from:owner,gas:3000000});
+// console.log(this.att.allowance(owner,contractAddress,{from:owner,gas:3000000}))
+// console.log(this.att.balanceOf(owner))
+// this.bill.billing(owner,{from:owner,gas:3000000});
+// this.bill.deductFee(1002,{from:owner,gas:3000000});
+
+//过滤log1 "weige"的事件
+// let hex = '0x' + Buffer.from("weige", 'utf8').toString("hex");
+// for(let i = hex.length; i < 66; i++) {
+//     hex += '0';
+// }
+// const filter = web3.eth.filter({topics: [hex]});
+// filter.watch(function (error, log) {
+//     console.log(log);  
+//     filter.stopWatching();      
+// });
